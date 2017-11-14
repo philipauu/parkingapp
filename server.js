da484d11fcd2df152ca20c4bd384c4257d0f85da
philipauu <Phil@Phils-MacBook.local>
2017-11-14 10:36:45 +0800

    Sho' they got to have it against the law.  Shoot, ever'body git high, they wouldn't be nobody git up and feed the chickens.  Hee-hee. 		-- Terry Southern

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ require('./config/passports/main_passport')(passport); // pass passport for conf
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
